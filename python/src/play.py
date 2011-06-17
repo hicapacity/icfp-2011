d758504126dd073c55fa00b32054fd2fbb8896fb
Jason Axelson <jason.axelson@gmail.com>
2011-06-16 20:57:24 -1000

    Python should actually play

--- python/src/play.py
+++ python/src/play.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python
 import sys
-print "I am player %s" % sys.argv[1]
+playerNum = sys.argv[1]
+print "I am player %s" % playerNum
 
+def readOpponentTurn():
+    opp1 = raw_input()
+    opp2 = raw_input()
+    opp3 = raw_input()
+
+def double(slot):
+    print "1"
+    print "dbl"
+    print slot
+
+def zero(slot):
+    print "2"
+    print slot
+    print zero
+
+def succ(slot);
+    print "1"
+    print "succ"
+    print slot
+
+def dec(slot):
+    print "1"
+    print "dec"
+    print slot
+
+if(playerNum == 1):
+    readOpponentTurn()
+
+turnNum = 0
+while(1 == 1):
+    turnNum = turnNum + 1
+    if(turnNum == 0):
+        zero()
+    elif(turnNum == 1):
+        succ()
+    elif(turnNum <= 6):
+        dbl()
+    elif(turnNum >= 7):
+        dec()
+        turnNum = 0
