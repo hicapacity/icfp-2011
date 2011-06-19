3e78230d38f1b817914784adc9ccd27f26ab9bfa
Jason Axelson <jason.axelson@gmail.com>
2011-06-19 09:18:33 -1000

    More backend functions

--- python/src/icfp.py
+++ python/src/icfp.py
@@ -3,10 +3,21 @@ def readOpponentTurn():
     opp2 = raw_input()
     opp3 = raw_input()
 
+def dbl1(slot):
+    print "1"
+    print "dbl"
+    print slot
+
 def dbl(slot):
     print "1"
     print "dbl"
     print slot
+
+def zero2(slot):
+    #print >> sys.stderr, "zero!"
+    print "2"
+    print slot
+    print "zero"
 
 def zero(slot):
     #print >> sys.stderr, "zero!"
@@ -15,6 +26,11 @@ def zero(slot):
     print "zero"
 
 def succ(slot):
+    print "1"
+    print "succ"
+    print slot
+
+def succ1(slot):
     print "1"
     print "succ"
     print slot
